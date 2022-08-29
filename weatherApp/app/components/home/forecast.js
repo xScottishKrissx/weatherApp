3b965bd2e9209a93ff0ee284392636bb13bd522f
Chris Dunne <chrisdunne66@gmail.com>
2022-08-29 16:26:48 +0100

    0.22 - Creating collapsible forecast items
    ...where you can click on a row of the forecast and get extra information

--- weatherApp/app/components/home/forecast.js
+++ weatherApp/app/components/home/forecast.js
@@ -1,8 +1,30 @@
-import React from 'react'
-import { StyleSheet, Text, View, Image, ImageBackground, ScrollView, SectionList, FlatList } from 'react-native';
+import React,{useState, useRef, useEffect} from 'react'
+import { StyleSheet, Text, View, Image, ImageBackground, ScrollView, SectionList, FlatList, TouchableOpacity } from 'react-native';
 import colours from '../../config/colours';
 
 export default function Forecast({apiData}) {
+    // const [toggleExtraInfo, setTogggleExtraInfo] = useState(false)
+
+    const forecastRef = useRef(null)
+
+    // useEffect(() =>{
+    //     console.log(forecastRef)
+    // },[forecastRef])
+    // const onPress = () => setTogggleExtraInfo(true)
+
+    const [isActive, setIsActive] = useState(false)
+    function toggleQuestions(questionIndex){
+        // console.log(isActive + " r" + questionIndex)
+
+        if(isActive === questionIndex){
+            setIsActive(false)
+        }else{
+            setIsActive(questionIndex)
+        }
+    }
+
+
+
   return (
     <>
         <Text style={{fontSize:20}}>Forecast</Text>
@@ -32,43 +54,51 @@ export default function Forecast({apiData}) {
                 const getTime = new Date(item.dt * 1000).toLocaleTimeString([], timeOptions)
 
                 const {temp, humidity} = item.main
-                const{description} = item.weather[0]
+                const {description} = item.weather[0]
 
                 const icon = apiData.list[index].weather[0].icon
-                console.log(icon)
+                // console.log(icon)
                 // if(getTime.includes("AM"))icon.replace('d', 'n')
                 // if(getTime.includes("PM"))icon.replace('n', 'd')
                 
                 
-                
+                // const thingy = index === isActive ? "showRow" : null
 
                 return(
-                    
-                        <View style={[styles.forecastItemRow]} key={index}>
-                            
-                            <View style={styles.forecastItem}>
-                                <Text>{displayDate}</Text>
-                            </View>
+                    <TouchableOpacity onPress={()=> toggleQuestions(index)} activeOpacity={2}>
 
-                            <View style={styles.forecastItem}>
-                                <Text>{getTime}</Text>
-                            </View>
-                            
-                            <View style={styles.forecastItem}>
-                                <Text>{temp}&#176;c</Text>
-                            </View>
+                       
 
-                            <View style={styles.forecastItem}>
-                                {/* <Text style={{textAlign:"center"}}>{description}</Text> */}
-                                <Image source={{uri:'http://openweathermap.org/img/wn/' + icon + '@2x.png'}} style={{width:50, height:50}} />
-                            </View>
+                        <View style={[styles.forecastItemRow, index === isActive ? styles.showRow : null ]} ref={forecastRef} key={index} onPress={() => toggleQuestions(index)}>
+                        {/* <View style={[styles.forecastItemRow]} ref={forecastRef} key={index} onPress={() => toggleQuestions(index)}> */}
+                        {/* <View style={[styles.forecastItemrow],`forecastItemRow ${index === toggleExtraInfo? "showRow" : null}`} ref={forecastRef} key={index}> */}
+                                
+                                <View style={styles.forecastItem}>
+                                    <Text>{displayDate}</Text>
+                                </View>
 
-                            <View style={styles.forecastItem}>
-                                <Text>Humidity</Text>
-                                <Text>{humidity}%</Text>
-                            </View>
+                                <View style={styles.forecastItem}>
+                                    <Text>{getTime}</Text>
+                                </View>
+                                
+                                <View style={styles.forecastItem}>
+                                    <Text>{temp}&#176;c</Text>
+                                    {index === isActive ? <Text style={{fontSize:10}}>Feels like 15</Text> : null}
+                                    
+                                </View>
 
-                        </View>
+                                <View style={styles.forecastItem}>
+                                    <Image source={{uri:'http://openweathermap.org/img/wn/' + icon + '@2x.png'}} style={{width:50, height:50}} />
+                                    <Text style={{textAlign:"center", fontSize:10, opacity:0}}>{description}</Text>
+                                </View>
+
+                                <View style={styles.forecastItem}>
+                                    <Text>{humidity}%</Text>
+                                    <Text style={{fontSize:10, opacity:0}}>Humidity</Text>
+                                </View>
+
+                            </View>
+                        </TouchableOpacity>
                     )
                 }
                 
@@ -91,5 +121,9 @@ const styles = StyleSheet.create({
     borderTest:{
         borderWidth:1,
         borderColor:"red",
+    },
+    showRow:{
+        height:100,
+        backgroundColor:"red"
     }
 })
